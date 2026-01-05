9ee634ddbc2ea30a29c753c3acaae5cabc38a1a9
NikkoRagudo <168717963+DevNikz@users.noreply.github.com>
2026-01-05 10:59:52 +0800

    nonagon (9 sided polygon)

--- GRAPH-Master1.cpp
+++ GRAPH-Master1.cpp
@@ -10,7 +10,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Nikko Ragudo", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -26,11 +26,33 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Render an nth sided polygon */
+        glColor3f(1.0f, 1.0f, 1.0f); //Set Color to white | RGB
+
+        float x = 0.0f;
+        float y = 0.0f;
+        float radius = 0.5f;
+        int num_segments = 9;
+
+        glBegin(GL_TRIANGLE_FAN);
+        glVertex2f(x, y);
+
+        for (int i = 0; i <= num_segments; ++i) {
+            float angle = i * 2.0f * PI / num_segments;
+            glVertex2f(x + radius * cos(angle), y + radius * sin(angle));
+        }
+
+        glEnd();
+        glFlush();
+
+        /* Render Triangle*/
+        /*
         glBegin(GL_TRIANGLES);
         glVertex2f(-0.5f, -0.5f);
         glVertex2f(0.0f, 0.5f);
         glVertex2f(0.5f, -0.5f);
         glEnd();
+        */
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
--- GRAPH-Master1.h
+++ GRAPH-Master1.h
@@ -2,3 +2,5 @@
 
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <math.h>
+#define PI 3.14159265358979323846
